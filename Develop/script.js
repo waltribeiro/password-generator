3068d19103f8fe5fe7d31b8551a04e8d06e560c8
WaltRibeiro <walt@fororchestra.com>
2020-02-07 13:48:25 -0500

    moved confirms inside myButton()

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,64 @@
-// 1.) should I be writing pseudo code before I code?
-// 2.) how can I bind the character select to the 
-
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var symbols = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbolsArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 
-var complexity = document.getElementById("value").value;
+// I'm trying to tie myButton() to the HTML's generate button
 
-
-function generate() {
-
+function myButton() {
+  var uppercaseInclude = confirm("Include uppercase?");
+  var lowercaseInclude = confirm("Include lowercase?");
+  var numbersInclude = confirm("Include numbers?");
+  var symbolsInclude = confirm("Include symbols?");
 }
 
+// 4 functions that I will .concat or .append once my code works
 
-
-
-
-
-
-document.getElementById("demo").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+function uppercaseIndex() {
+  return Math.floor(Math.random() * uppercaseArray.length);
 }
 
-
-generateBtn.addEventListener("click", writePassword);
-
-
-if (passwordLength > 8 && passwordLength <= 128) {
-  uppercaseCapture = [Math.floor(Math.random() * uppercase.length)];
+function lowercaseIndex() {
+  return Math.floor(Math.random() * lowercaseArray.length);
 }
 
-console.log(uppercaseCapture);
+function numbersIndex() {
+  return Math.floor(Math.random() * numbersArray.length);
+}
 
-// if (uppercaseAnswer = true || lowercaseAnswer = true || numbersAnswer = true || ) {
+function symbolsIndex() {
+  return Math.floor(Math.random() * symbolsArray.length);
+}
+
+// checking the console to assure that my functions work
+
+console.log(uppercaseArray[uppercaseIndex()]);
+console.log(lowercaseArray[lowercaseIndex()]);
+console.log(numbersArray[numbersIndex()]);
+console.log(symbolsArray[symbolsIndex()]);
+
+document.getElementById("generate").onclick = function() {
+  uppercaseIndex();
+};
+
+function passwordQuestion() {
+  if (passwordLength > 8 && passwordLength <= 128) {
+    uppercaseCapture = [Math.floor(Math.random() * uppercase.length)];
+  }
+
+  console.log(uppercaseArray[uppercaseIndex()]);
+  console.log(lowercaseArray[lowercaseIndex()]);
+  console.log(numbersArray[numbersIndex()]);
+  console.log(symbolsArray[symbolsIndex()]);
+}
+
+generateBtn.addEventListener("click", uppercaseIndex);
+
+// PSEUDO CODE THAT IS NOT WORKING AS INTENDED
+
+//if (uppercaseAnswer = true || lowercaseAnswer = true || numbersAnswer = true || ) {
 //   var uppercaseTotal = uppercase[Math.floor(Math.random() * uppercase.length)];
 // }
 
@@ -55,47 +75,17 @@ console.log(uppercaseCapture);
 //   var specialsTotal = specials[Math.floor(Math.random() * specials.length)];
 // }
 
-console.log(uppercaseAnswer);
-console.log(lowercaseAnswer);
-console.log(numbersAnswer);
-console.log(symbolsAnswer);
 
-function characterTotalFunction (characterTotal) {
-  characterTotal * uppercaseFunctionTotal
-}
-if (document.getElementById("numbers") == true) {
-  
-}
+  // var uppercaseFunctionTotal = function uppercaseFunction() {
 
-var uppercaseFunctionTotal = function uppercaseFunction() {
   //  uppercase[Math.floor(Math.random() * Array.length) + 1]
-  // return 
-}
-
+  // return
+  // };
 
 // Write password to the #password input
-function writePassword() {
-  console.log("Hello");
 
-  console.log(uppercase[Math.floor(Math.random() * uppercase.length)]);
-  console.log(lowercase[Math.floor(Math.random() * lowercase.length)]);
-  console.log(numbers[Math.floor(Math.random() * numbers.length)]);
-  console.log(symbols[Math.floor(Math.random() * symbols.length)]);
+// uppercaseRandom * document.getElementById("value")
 
-  function myFunction() {
-    var numbers = ["1", "2", "3"];
-    var uppercase = ["A", "B", "C"];
-      var lowercase = ["a", "b", "c"];
-    var children = numbers.concat(uppercase).concat(lowercase); 
-    document.getElementById("demo").innerHTML = children;
-  }
-
-  // uppercaseRandom * document.getElementById("value")
-
-  var passwordLengthSelect = document.getElementById("passwordLength");
-  var passwordLengthValue = passwordLengthSelect.options[passwordLengthSelect.selectedIndex].value;
-
-  // var uppercaseRandomTotal = uppercaseRandom + lowercaseRandom + numbersRandom + specialsRandom
-}
+// var uppercaseRandomTotal = uppercaseRandom + lowercaseRandom + numbersRandom + specialsRandom
 
 // Add event listener to generate button
